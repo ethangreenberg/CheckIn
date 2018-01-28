25649969a0b97a72c23b0a930e8149e3378daca1
Katerina R <katerina.rusa@yahoo.com>
2018-01-28 09:56:06 -0500

    Fixed the merge error

--- CheckIn/CheckIn/ViewController.swift
+++ CheckIn/CheckIn/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func updateTime(_ sender: UIDatePicker) {
+        
+    }
+    @IBAction func AddTime(_ sender: Any) {
+        
+    }
     
 }
 
